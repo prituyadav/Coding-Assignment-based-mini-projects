bb5c5d535aaaeb63e361be14f5385e2f463d6da6
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-02-26 09:26:42 +0530

    Add files via upload

---
+++ SpringTest-1/src/main/java/com/revision/SpringTest1Application.java
@@ -0,0 +1,13 @@
+package com.revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest1Application.class, args);
+	}
+
+}
---
+++ SpringTest-1/src/test/java/com/revision/SpringTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
