b476a58fedee20d652d9490e1b6900901b0b5f9b
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-02-26 09:24:42 +0530

    Add files via upload

---
+++ Abc/src/check/Student.java
@@ -0,0 +1,41 @@
+package check;
+
+
+class Student implements Runnable{
+	
+	private Thread t;
+	private String threadName;
+		
+	public Student(String threadName) {
+		// TODO Auto-generated constructor stub
+		this.threadName=threadName;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true)
+			System.out.println(threadName);
+		
+	}
+	
+	public void start() {
+		if(t==null) {
+			t=new Thread(this,threadName);
+			t.start();
+		}
+	}
+       
+	
+	public static class M {
+		private static void main(String[] arr) {
+			// TODO Auto-generated method stub
+            Student a=new Student("A");
+            Student b=new Student("B");
+            
+            b.start();
+            a.start();
+		}
+	}
+	
+}
