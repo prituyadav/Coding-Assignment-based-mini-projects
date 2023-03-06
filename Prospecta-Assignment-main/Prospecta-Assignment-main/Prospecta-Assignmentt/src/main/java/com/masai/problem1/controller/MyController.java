16342fdfd30d0fe670e436be137829f6a2f3c49f
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-03-06 23:48:16 +0530

    Update MyController.java

--- Prospecta-Assignment-main/Prospecta-Assignment-main/Prospecta-Assignmentt/src/main/java/com/masai/problem1/controller/MyController.java
+++ Prospecta-Assignment-main/Prospecta-Assignment-main/Prospecta-Assignmentt/src/main/java/com/masai/problem1/controller/MyController.java
@@ -1,19 +1,3 @@
-package com.masai.problem1.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import com.masai.problem1.exception.EntryException;
-import com.masai.problem1.model.Entry;
-import com.masai.problem1.model.EntryDto;
-import com.masai.problem1.service.EntryService;
 
 @RestController
 public class MyController {
